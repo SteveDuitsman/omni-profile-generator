19977af5a2e9b850e950317fb41252efc4b36827
Steve Duitsman <steve.duitsman@omniresources.com>
2017-03-08 17:03:08 -0600

    Fix the build.

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
-namespace ProfileGenerator
+namespace OmniProfileGenerator
 {
     public class Program
     {
